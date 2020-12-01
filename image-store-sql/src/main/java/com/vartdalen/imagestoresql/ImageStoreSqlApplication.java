c4658e6c612c3263ff581ec8d53d616fda8c114c
vartdalen <viktor.v.johansen@hotmail.com>
2020-12-02 00:02:10 +0100

    Create image-store-sql project

---
+++ image-store-sql/src/main/java/com/vartdalen/imagestoresql/ImageStoreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.vartdalen.imagestoresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageStoreSqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageStoreSqlApplication.class, args);
+    }
+
+}
---
+++ image-store-sql/src/main/java/com/vartdalen/imagestoresql/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.vartdalen.imagestoresql;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ImageStoreSqlApplication.class);
+    }
+
+}
