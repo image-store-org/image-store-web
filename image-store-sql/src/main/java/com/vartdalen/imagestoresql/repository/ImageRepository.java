00ef83edc66e79d7da9fbf800304bf2a55e31619
vartdalen <viktor.v.johansen@hotmail.com>
2020-12-10 20:23:24 +0100

    Remove image-store-sql files

--- image-store-sql/src/main/java/com/vartdalen/imagestoresql/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vartdalen.imagestoresql.repository;
-
-import com.vartdalen.imagestoresql.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
--- image-store-sql/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO image (id, title, category, bytes, created)
-VALUES
-(1, 'testTitle', 'testCategory', 01010101, '2019-07-13 12:00:00');
