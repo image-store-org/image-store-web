b04e671b26469987a5a9aff52ec74f5986f8f16f
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-05 21:28:57 +0100

    Update Image model

---
+++ image-store-web/src/main/java/com/vartdalen/imagestoreweb/model/enumerator/ImageCategory.java
@@ -0,0 +1,20 @@
+package com.vartdalen.imagestoreweb.model.enumerator;
+
+public enum ImageCategory {
+    NONE,
+    ABSTRACT,
+    ANIMAL,
+    ARCHITECTURE,
+    BACKGROUND,
+    CALENDAR,
+    CULTURE,
+    FAMILY,
+    NATURE,
+    PEOPLE,
+    RECREATION,
+    RELIGION,
+    SPORT,
+    TECHNOLOGY,
+    VACATION,
+    WORK,
+}
---
+++ image-store-web/src/main/java/com/vartdalen/imagestoreweb/model/enumerator/ImageOrientation.java
@@ -0,0 +1,7 @@
+package com.vartdalen.imagestoreweb.model.enumerator;
+
+public enum ImageOrientation {
+    LANDSCAPE,
+    PORTRAIT,
+    SQUARE
+}
