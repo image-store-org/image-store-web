89aa7688838c78c0baf7bddced079c15bfb478cf
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-08 20:37:46 +0100

    Create HTTPS redirect

--- image-store-web/src/main/java/com/vartdalen/imagestoreweb/ImageStoreWebApplication.java
+++ image-store-web/src/main/java/com/vartdalen/imagestoreweb/ImageStoreWebApplication.java
@@ -1,12 +1,52 @@
 package com.vartdalen.imagestoreweb;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ImageStoreWebApplication {
 
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Value("${server.port}")
+    private int httpsPort;
+
     public static void main(String[] args) {
         SpringApplication.run(ImageStoreWebApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(httpPort);
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
     }
 }
