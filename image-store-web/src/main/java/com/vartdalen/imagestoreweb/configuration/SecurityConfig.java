2449ad3554e59eecf4a40e4d65b6ad3db6bfd3d9
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-13 00:19:10 +0100

    Create API authorization

--- image-store-web/src/main/java/com/vartdalen/imagestoreweb/configuration/SecurityConfig.java
+++ image-store-web/src/main/java/com/vartdalen/imagestoreweb/configuration/SecurityConfig.java
@@ -1,18 +1,49 @@
 package com.vartdalen.imagestoreweb.configuration;
 
+import com.vartdalen.imagestoreweb.filter.ApiAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${server.api.authorization.header}")
+    private String authorizationHeader;
+
+    @Value("${server.api.authorization.token}")
+    private String authorizationToken;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        ApiAuthorizationFilter filter = new ApiAuthorizationFilter(authorizationHeader);
+        filter.setAuthenticationManager(authentication -> {
+            String principal = (String) authentication.getPrincipal();
+            if (!authorizationToken.equals(principal))
+            {
+                throw new BadCredentialsException("The API key was not found or not the expected value.");
+            }
+            authentication.setAuthenticated(true);
+            return authentication;
+        });
+
         http
                 .requiresChannel()
                 .anyRequest()
-                .requiresSecure()
-                .and().csrf().disable();
+                .requiresSecure();
+        http
+                .antMatcher("/api/**")
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).
+                and()
+                .addFilter(filter)
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
     }
 }
