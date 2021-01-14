dfd10fc62ec7822716516fe2e3bd471403ae5fc6
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-14 22:14:13 +0100

    Enhance authorization

--- image-store-web/src/main/java/com/vartdalen/imagestoreweb/configuration/SecurityConfig.java
+++ image-store-web/src/main/java/com/vartdalen/imagestoreweb/configuration/SecurityConfig.java
@@ -3,25 +3,26 @@ package com.vartdalen.imagestoreweb.configuration;
 import com.vartdalen.imagestoreweb.filter.ApiAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Value("${server.api.authorization.header}")
-    private String authorizationHeader;
+    private String AUTHORIZATION_HEADER;
 
     @Value("${server.api.authorization.token}")
-    private String authorizationToken;
+    private String AUTHORIZATION_TOKEN;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        ApiAuthorizationFilter filter = new ApiAuthorizationFilter(authorizationHeader, authorizationToken);
+
+        ApiAuthorizationFilter filter = new ApiAuthorizationFilter(AUTHORIZATION_HEADER, AUTHORIZATION_TOKEN);
 
         http
                 .requiresChannel()
@@ -39,12 +40,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .exceptionHandling()
-                .authenticationEntryPoint((request, response, e) ->
-                {
-                    response.setContentType("application/json;charset=UTF-8");
-                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                    response.getWriter().write("Invalid API authentication token");
-                });;
-
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
     }
 }
--- image-store-web/src/main/java/com/vartdalen/imagestoreweb/filter/ApiAuthorizationFilter.java
+++ image-store-web/src/main/java/com/vartdalen/imagestoreweb/filter/ApiAuthorizationFilter.java
@@ -6,22 +6,22 @@ import javax.servlet.http.HttpServletRequest;
 
 public class ApiAuthorizationFilter extends AbstractPreAuthenticatedProcessingFilter {
 
-    private final String authorizationHeader;
-    private final String authorizationToken;
+    private final String AUTHORIZATION_HEADER;
+    private final String AUTHORIZATION_TOKEN;
 
     public ApiAuthorizationFilter(String authorizationHeader, String authorizationToken) {
-        this.authorizationHeader = authorizationHeader;
-        this.authorizationToken = authorizationToken;
+        this.AUTHORIZATION_HEADER = authorizationHeader;
+        this.AUTHORIZATION_TOKEN = authorizationToken;
         this.setAuthenticationManager(manager -> {
             String principal = (String) manager.getPrincipal();
-            manager.setAuthenticated(this.authorizationToken.equals(principal));
+            manager.setAuthenticated(this.AUTHORIZATION_TOKEN.equals(principal));
             return manager;
         });
     }
 
     @Override
     protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
-        return request.getHeader(authorizationHeader);
+        return request.getHeader(AUTHORIZATION_HEADER);
     }
 
     protected Object getPreAuthenticatedCredentials(HttpServletRequest request) { return null; }
