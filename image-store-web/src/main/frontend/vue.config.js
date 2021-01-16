fc2085a9a92c953e98ac7c6536bd60d332a87625
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-16 22:31:07 +0100

    Create pre-loader Create Loader component

--- image-store-web/src/main/frontend/vue.config.js
+++ image-store-web/src/main/frontend/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
 	outputDir: '../../../target/classes/public',
+	chainWebpack: config => {
+		config.module
+			.rule("vue")
+			.use("vue-svg-inline-loader")
+				.loader("vue-svg-inline-loader");
+	}
 }
