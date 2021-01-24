1a656dfbb146f18db76d61d3dab45a08c635d206
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-24 14:19:47 +0100

    Enhance theme

--- image-store-web/src/main/frontend/src/main.ts
+++ image-store-web/src/main/frontend/src/main.ts
@@ -1,4 +1,4 @@
-import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 
