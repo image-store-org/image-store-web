f00328de5565329b14cfefb0cbef4171eba0ec8c
vartdalen <viktor.v.johansen@hotmail.com>
2021-01-24 14:02:27 +0100

    Create Navbar.vue Enhance navbar

--- image-store-web/src/main/frontend/src/App.vue
+++ image-store-web/src/main/frontend/src/App.vue
@@ -5,14 +5,7 @@
         />
         <div v-else
              id="nav">
-            <menubar :model="items">
-                <template #start>
-                    <img alt="logo" src="./assets/logo.png" height="40" class="p-mr-2">
-                </template>
-                <template #end>
-                    <Button label="Logout" icon="pi pi-power-off" :style="{'margin-left': '0 .5em'}"/>
-                </template>
-            </menubar>
+            <navbar />
             <router-link to="/">
                 Home
             </router-link> |
@@ -20,17 +13,17 @@
                 About
             </router-link>
         </div>
-        <router-view v-if="!isLoading"/>
+        <router-view v-if="!isLoading" />
     </div>
 </template>
 
 <script>
     import Loader from "@/components/Loader";
     import {ref} from "vue";
-    import Menubar from 'primevue/menubar';
+    import Navbar from "@/components/Navbar";
 
     export default {
-        components: {Loader, Menubar},
+        components: {Navbar, Loader },
         setup() {
             const isLoading = ref(true);
             document.onreadystatechange = () => {
@@ -40,137 +33,6 @@
             }
             return {
                 isLoading
-            }
-        },
-        data() {
-            return {
-                items: [
-                    {
-                        label:'File',
-                        icon:'pi pi-fw pi-file',
-                        items:[
-                            {
-                                label:'New',
-                                icon:'pi pi-fw pi-plus',
-                                items:[
-                                    {
-                                        label:'Bookmark',
-                                        icon:'pi pi-fw pi-bookmark'
-                                    },
-                                    {
-                                        label:'Video',
-                                        icon:'pi pi-fw pi-video'
-                                    },
-
-                                ]
-                            },
-                            {
-                                label:'Delete',
-                                icon:'pi pi-fw pi-trash'
-                            },
-                            {
-                                separator:true
-                            },
-                            {
-                                label:'Export',
-                                icon:'pi pi-fw pi-external-link'
-                            }
-                        ]
-                    },
-                    {
-                        label:'Edit',
-                        icon:'pi pi-fw pi-pencil',
-                        items:[
-                            {
-                                label:'Left',
-                                icon:'pi pi-fw pi-align-left'
-                            },
-                            {
-                                label:'Right',
-                                icon:'pi pi-fw pi-align-right'
-                            },
-                            {
-                                label:'Center',
-                                icon:'pi pi-fw pi-align-center'
-                            },
-                            {
-                                label:'Justify',
-                                icon:'pi pi-fw pi-align-justify'
-                            },
-
-                        ]
-                    },
-                    {
-                        label:'Users',
-                        icon:'pi pi-fw pi-user',
-                        items:[
-                            {
-                                label:'New',
-                                icon:'pi pi-fw pi-user-plus',
-
-                            },
-                            {
-                                label:'Delete',
-                                icon:'pi pi-fw pi-user-minus',
-
-                            },
-                            {
-                                label:'Search',
-                                icon:'pi pi-fw pi-users',
-                                items:[
-                                    {
-                                        label:'Filter',
-                                        icon:'pi pi-fw pi-filter',
-                                        items:[
-                                            {
-                                                label:'Print',
-                                                icon:'pi pi-fw pi-print'
-                                            }
-                                        ]
-                                    },
-                                    {
-                                        icon:'pi pi-fw pi-bars',
-                                        label:'List'
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        label:'Events',
-                        icon:'pi pi-fw pi-calendar',
-                        items:[
-                            {
-                                label:'Edit',
-                                icon:'pi pi-fw pi-pencil',
-                                items:[
-                                    {
-                                        label:'Save',
-                                        icon:'pi pi-fw pi-calendar-plus'
-                                    },
-                                    {
-                                        label:'Delete',
-                                        icon:'pi pi-fw pi-calendar-minus'
-                                    },
-                                ]
-                            },
-                            {
-                                label:'Archieve',
-                                icon:'pi pi-fw pi-calendar-times',
-                                items:[
-                                    {
-                                        label:'Remove',
-                                        icon:'pi pi-fw pi-calendar-minus'
-                                    }
-                                ]
-                            }
-                        ]
-                    },
-                    {
-                        label:'Quit',
-                        icon:'pi pi-fw pi-power-off'
-                    }
-                ]
             }
         }
     }
---
+++ image-store-web/src/main/frontend/src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+    <menubar :model="items">
+        <template #start>
+            <img alt="logo" src="../assets/logo.png" height="40" class="p-mr-2">
+        </template>
+    </menubar>
+</template>
+
+<script lang="ts">
+    import {defineComponent} from "vue";
+    import Menubar from "primevue/menubar";
+
+    export default defineComponent({
+        components: { Menubar },
+        data() {
+            return {
+                items: [
+                    {
+                        label:'File',
+                        icon:'pi pi-fw pi-file',
+                        items: [
+                            {
+                                label:'Upload',
+                                icon:'pi pi-fw pi-plus'
+                            },
+                            {
+                                label:'Browse',
+                                icon:'pi pi-fw pi-search'
+                            }
+                        ]
+                    },
+                    {
+                        label:'User',
+                        icon:'pi pi-fw pi-user',
+                        items:[
+                            {
+                                label:'Edit',
+                                icon:'pi pi-fw pi-user-edit',
+
+                            },
+                            {
+                                separator: true
+                            },
+                            {
+                                label:'Logout',
+                                icon:'pi pi-fw pi-power-off'
+                            }
+                        ]
+                    },
+                ]
+            }
+        }
+    });
+</script>
+
+<style lang="scss">
+    .p-menubar {
+        justify-content: space-between;
+    }
+    .p-menubar .p-menubar-root-list ul {
+        right: 0;
+        left: auto;
+    }
+    .p-menubar .p-menubar-root-list .p-menuitem .p-submenu-list .p-menuitem-icon.pi-power-off {
+        color: red;
+    }
+</style>
