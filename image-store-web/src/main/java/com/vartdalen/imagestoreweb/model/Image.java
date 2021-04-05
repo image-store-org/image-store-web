381ecacb7f9fa4a3c2ac90e67393619076679b0f
vartdalen <viktor.v.johansen@hotmail.com>
2021-04-05 12:34:11 +0200

    Enhance Image.java

--- image-store-web/src/main/java/com/vartdalen/imagestoreweb/model/Image.java
+++ image-store-web/src/main/java/com/vartdalen/imagestoreweb/model/Image.java
@@ -16,6 +16,9 @@ public class Image {
 
     @NotNull
     private String title;
+    
+    @NotNull
+    private ImageExtension extension;
 
     @NotNull
     private ImageOrientation orientation;
@@ -23,36 +26,33 @@ public class Image {
     @NotNull
     private Set<ImageCategory> categories;
 
-    @NotNull
-    private ImageExtension extension;
-
     private byte[] bytes;
 
     private LocalDateTime created;
 
     public Image() {}
 
-    public Image(long id, String title, ImageOrientation orientation, ImageCategory[] categories, ImageExtension extension) {
+    public Image(long id, String title, ImageExtension extension, ImageOrientation orientation, ImageCategory[] categories) {
         this.id = id;
         this.title = title;
+        this.extension = extension;
         this.orientation = orientation;
         this.categories = new HashSet<>(Arrays.asList(categories));
-        this.extension = extension;
     }
 
     public long getId() { return id; }
     public String getTitle() { return title; }
+    public ImageExtension getExtension() { return extension; }
     public ImageOrientation getOrientation() { return orientation; }
     public Set<ImageCategory> getCategories() { return categories; }
-    public ImageExtension getExtension() { return extension; }
     public byte[] getBytes() { return bytes; }
     public LocalDateTime getCreated() { return created; }
 
     public void setId(long id) { this.id = id; }
     public void setTitle(String title) { this.title = title; }
+    public void setExtension(ImageExtension extension) { this.extension = extension; }
     public void setOrientation(ImageOrientation orientation) { this.orientation = orientation; }
     public void setCategories(ImageCategory[] categories) { this.categories = new HashSet<>(Arrays.asList(categories)); }
-    public void setExtension(ImageExtension extension) { this.extension = extension; }
     public void setBytes(byte[] bytes) { this.bytes = bytes; }
 }
 
